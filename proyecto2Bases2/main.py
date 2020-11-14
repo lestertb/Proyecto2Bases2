c4523b8dab7275d20fc49d93b126989e963d0205
Marco <marcoespinozasalas@gmail.com>
2020-11-13 23:30:32 -0600

    ControlDeErrores

--- proyecto2Bases2/main.py
+++ proyecto2Bases2/main.py
@@ -10,9 +10,12 @@ from tkinter import *
 
 
 def connexion(server, database, username, password):
-    cnxn = pyodbc.connect(
-        'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
-    return cnxn
+    try:
+        cnxn = pyodbc.connect(
+            'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+        return cnxn
+    except pyodbc.Error as err:
+        tkinter.messagebox.showerror(title="Error", message=err)
 
 
 # cursor = cnxn.cursor()
@@ -52,16 +55,12 @@ def main():
     labelContra = Label(root, text="Contraseña")
     labelContra.place(x=125, y=170)
 
-    servers = Entry.get(e)
-    databases = Entry.get(e2)
-    usernames = Entry.get(e3)
-    passwords = Entry.get(e4)
+    server = Entry.get(e)
+    database = Entry.get(e2)
+    username = Entry.get(e3)
+    password = Entry.get(e4)
 
     def mycclick():
-        server = servers
-        database = databases
-        username = usernames
-        password = passwords
         try:
             conn = connexion(server, database, username, password)
             tkinter.messagebox.showinfo(title="Exito", message="Conectado con exito")
@@ -92,12 +91,15 @@ def ventanaDespues(conn):
     textoCentral.place(x=50, y=150)  # ubicacion en x y y
 
     def getSchemas():
-        cur = conn.cursor()
-        cur.execute("SELECT DISTINCT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA ")
-        data = []
-        for tabla_list in cur.fetchall():
-            data.append(tabla_list[0])
-        return data
+        try:
+            cur = conn.cursor()
+            cur.execute("SELECT DISTINCT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA ")
+            data = []
+            for tabla_list in cur.fetchall():
+                data.append(tabla_list[0])
+            return data
+        except pyodbc.Error as err:
+            tkinter.messagebox.showerror(title="Error", message=err)
 
     label = Label(newroot, text="Seleccione el esquema al cual pertenece la tabla")
     label.place(x=50, y=60)
@@ -107,94 +109,115 @@ def ventanaDespues(conn):
     comboBoxAttributes = ttk.Combobox(newroot, width=40)
 
     def callback(eventObject):
-        comboBoxTables.set('')
-        resultado()
+        try:
+            comboBoxTables.set('')
+            resultado()
+        except RuntimeError as err:
+            tkinter.messagebox.showerror(title="Error", message=err)
 
     def callback2(eventObject):
-        comboBoxAttributes.set('')
-        llenarComboBoxAtributos()
+        try:
+            comboBoxAttributes.set('')
+            llenarComboBoxAtributos()
+        except RuntimeError as err:
+            tkinter.messagebox.showerror(title="Error", message=err)
 
     comboBoxSchemas.bind("<<ComboboxSelected>>", callback)
     comboBoxTables.bind("<<ComboboxSelected>>", callback2)
-    def resultado():
-        if comboBoxSchemas.get() != '':
-            textoCentral.delete('1.0', END)
-            comboBoxTables["values"] = llenarComboBoxTablas(comboBoxSchemas.get())
-            comboBoxTables.place(x=1200, y=150)
-            labelTablas = Label(newroot, text="Seleccione la tabla con la que quiere trabajar")
-            labelTablas.place(x=1200, y=125)
-            privilegiosTabla(comboBoxTables.get())
 
-        else:
-            tkinter.messagebox.showinfo(title="Advertencia", message="Debe elegir un schema")
+    def resultado():
+        try:
+            if comboBoxSchemas.get() != '':
+                textoCentral.delete('1.0', END)
+                comboBoxTables["values"] = llenarComboBoxTablas(comboBoxSchemas.get())
+                comboBoxTables.place(x=1200, y=150)
+                labelTablas = Label(newroot, text="Seleccione la tabla con la que quiere trabajar")
+                labelTablas.place(x=1200, y=125)
+                privilegiosTabla(comboBoxTables.get())
+            else:
+                tkinter.messagebox.showinfo(title="Advertencia", message="Debe elegir un schema")
+        except AttributeError as err:
+            tkinter.messagebox.showerror(title="Error", message=err)
 
     def llenarComboBoxTablas(valSchema):
-        cur = conn.cursor()
-        cur.execute(
-            '''SELECT DISTINCT [TABLE_NAME] FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ? and TABLE_TYPE = 'BASE TABLE' ''',
-            valSchema)
-        data = []
-        for val in cur.fetchall():
-            data.append(val[0])
-        return data
+        try:
+            cur = conn.cursor()
+            cur.execute(
+                '''SELECT  DISTINCT [TABLE_NAME] FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ? and TABLE_TYPE = 'BASE TABLE' ''',
+                valSchema)
+            data = []
+            for val in cur.fetchall():
+                data.append(val[0])
+            return data
+        except pyodbc.Error as err:
+            tkinter.messagebox.showerror(title="Error", message=err)
 
     def privilegiosTabla(valTable):
         if valTable != '':
-            testString = "sp_table_privileges " + valTable
-            cur = conn.cursor()
-            cur.execute(testString)
-            for rows in cur:
-                info = "TABLE_QUALIFER" + " " + rows[0] + " " + "TABLE_NAME" + " " + rows[2] + " " + \
-                       "Privilige" + " " + rows[5]
-                info += '\n'
-                info += '\n'
-                textoCentral.insert(END, info)
-            # labelAtributos = Label(newroot, text="Seleccione el atributo con el que quiere trabajar")
-            # labelAtributos.place(x=1200, y=230)
-            # comboBoxAttributes["values"] = llenarComboAtributos(valTable)
-            # comboBoxAttributes.place(x=1200, y=250)
+            try:
+                testString = "sp_table_privileges " + valTable
+                cur = conn.cursor()
+                cur.execute(testString)
+                for rows in cur:
+                    info = "TABLE_QUALIFER" + " " + rows[0] + " " + "TABLE_NAME" + " " + rows[2] + " " + \
+                           "Privilige" + " " + rows[5]
+                    info += '\n'
+                    info += '\n'
+                    textoCentral.insert(END, info)
+            except pyodbc.Error as err:
+                tkinter.messagebox.showerror(title="Error", message=err)
         else:
             tkinter.messagebox.showinfo(title="Advertencia", message="Debe elegir una tabla")
 
     def llenarComboAtributos(tabla):
-        cur = conn.cursor()
-        cur.execute(
-            '''SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ? ''',
-            tabla)
-        data = []
-        for val in cur.fetchall():
-            data.append(val[0])
-        return data
+        try:
+            cur = conn.cursor()
+            cur.execute(
+                '''SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ? ''',
+                tabla)
+            data = []
+            for val in cur.fetchall():
+                data.append(val[0])
+            return data
+        except pyodbc.Error as err:
+            tkinter.messagebox.showerror(title="Error", message=err)
 
     def atributosTabla():
-        if comboBoxTables.get() != '' and comboBoxAttributes.get() != '':
-            textoCentral.delete('1.0', END)
-            verAtributosText(comboBoxTables.get(), comboBoxAttributes.get())
-        else:
-            tkinter.messagebox.showinfo(title="Advertencia", message="Debe elegir una tabla y un atributo")
+        try:
+            if comboBoxTables.get() != '' and comboBoxAttributes.get() != '':
+                textoCentral.delete('1.0', END)
+                verAtributosText(comboBoxTables.get(), comboBoxAttributes.get())
+            else:
+                tkinter.messagebox.showinfo(title="Advertencia", message="Debe elegir una tabla y un atributo")
+        except AttributeError as err:
+            tkinter.messagebox.showerror(title="Error", message=err)
 
     def verAtributosText(valTable, column):
         if valTable != '' and column != '':
-            string = "sp_column_privileges " + valTable + " ,NULL " + " ,NULL " + ',' + column
-            cur = conn.cursor()
-            cur.execute(string)
-            for rows in cur:
-                string1 = "TABLE_QUALIFER =" + " " + rows[0] + " - " + "TABLE_NAME =" + " " + rows[2] + " - " \
-                          + "COLUMN_NAME =" + " " + rows[3] + " - " + \
-                          "Privilige =" + " " + rows[6]
-                string1 += '\n'
-                string1 += '\n'
-                textoCentral.insert(END, string1)
-
+            try:
+                string = "sp_column_privileges " + valTable + " ,NULL " + " ,NULL " + ',' + column
+                cur = conn.cursor()
+                cur.execute(string)
+                for rows in cur:
+                    string1 = "TABLE_QUALIFER =" + " " + rows[0] + " - " + "TABLE_NAME =" + " " + rows[2] + " - " \
+                              + "COLUMN_NAME =" + " " + rows[3] + " - " + \
+                              "Privilige =" + " " + rows[6]
+                    string1 += '\n'
+                    string1 += '\n'
+                    textoCentral.insert(END, string1)
+            except pyodbc.Error as err:
+                tkinter.messagebox.showerror(title="Error", message=err)
         else:
             tkinter.messagebox.showinfo(title="Advertencia", message="Debe elegir una tabla y un atributo")
 
     def llenarComboBoxAtributos():
-
-        labelAtributos = Label(newroot, text="Seleccione el atributo con el que quiere trabajar")
-        labelAtributos.place(x=1200, y=230)
-        comboBoxAttributes["values"] = llenarComboAtributos(comboBoxTables.get())
-        comboBoxAttributes.place(x=1200, y=250)
+        try:
+            labelAtributos = Label(newroot, text="Seleccione el atributo con el que quiere trabajar")
+            labelAtributos.place(x=1200, y=230)
+            comboBoxAttributes["values"] = llenarComboAtributos(comboBoxTables.get())
+            comboBoxAttributes.place(x=1200, y=250)
+        except AttributeError as err:
+            tkinter.messagebox.showerror(title="Error", message=err)
 
     boton1 = Button(newroot, text="Ver privilegios de la tabla", command=resultado)
     boton1.place(x=1200, y=180)
